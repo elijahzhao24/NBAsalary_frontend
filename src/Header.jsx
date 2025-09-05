1dd51f8f4621ff38101a453f5a972e9314ccbe3f
Elijah Zhao <elijahzhao24@gmail.com>
2025-09-04 22:00:58 -0600

    Fix case sensitivity for logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import Logo from './assets/Logo.png';
+import Logo from './assets/logo.png';
 import LogoText from './assets/fulllogo.png';
 import { FiGithub, FiChevronDown } from 'react-icons/fi';
 import TeamDropdown from './TeamDropdown';
